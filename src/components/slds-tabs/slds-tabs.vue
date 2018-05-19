207b70c331299dd3c870e9bfde0e60fe6f1e4b5b
Guto <luiz.v.nasc@gmail.com>
2018-05-18 21:56:26 -0300

    criado propriedade que deixa tabs bloqueadas para leitura apenas

--- src/components/slds-tabs/slds-tabs.vue
+++ src/components/slds-tabs/slds-tabs.vue
@@ -11,8 +11,8 @@
                 <a v-html="tab.header"
                    :aria-controls="tab.hash"
                    :aria-selected="tab.isActive"
-                   @click="selectTab(tab.hash, $event)"
-                   :href="tab.hash"
+                   @click=" selectTabAction(tab.hash, $event)"
+                   :href="!readonly? tab.hash: ''"
                    class="slds-tabs_default__link"
                    role="tab"
                 >{{tab.header}}</a>
@@ -24,88 +24,112 @@
     </div>
 </template>
 <script>
-import expiringStorage from './expiringStorage';
-    export default {
-        props: {
-            cacheLifetime: {
-                default: 5,
-            },
-            options: {
-                type: Object,
-                required: false,
-                default: () => ({
-                    useUrlFragment: true,
-                }),
-            },
-        },
-        data: () => ({
-            tabs: [],
-            activeTabHash: '',
-        }),
-        computed: {
-            storageKey() {
-                return `vue-tabs-component.cache.${window.location.host}${window.location.pathname}`;
-            },
-        },
-        created() {
-            this.tabs = this.$children;
-        },
-        mounted() {
-            window.addEventListener('hashchange', () => this.selectTab(window.location.hash));
-            if (this.findTab(window.location.hash)) {
-                this.selectTab(window.location.hash);
-                return;
-            }
-            const previousSelectedTabHash = expiringStorage.get(this.storageKey);
-            if (this.findTab(previousSelectedTabHash)) {
-                this.selectTab(previousSelectedTabHash);
-                return;
-            }
-            if (this.tabs.length) {
-                this.selectTab(this.tabs[0].hash);
-            }
-        },
-        methods: {
-            findTab(hash) {
-                return this.tabs.find(tab => tab.hash === hash);
-            },
-            selectTab(selectedTabHash, event) {
-                // See if we should store the hash in the url fragment.
-                if (event && !this.options.useUrlFragment) {
-                    event.preventDefault();
-                }
-                const selectedTab = this.findTab(selectedTabHash);
-                if (! selectedTab) {
-                    return;
-                }
-                if (selectedTab.isDisabled) {
-                    return;
-                }
-                this.tabs.forEach(tab => {
-                    tab.isActive = (tab.hash === selectedTab.hash);
-                });
-                this.$emit('changed', { tab: selectedTab });
-                this.activeTabHash = selectedTab.hash;
-                expiringStorage.set(this.storageKey, selectedTab.hash, this.cacheLifetime);
-            },
-            setTabVisible(hash, visible) {
-                const tab = this.findTab(hash);
-                if (! tab) {
-                    return;
-                }
-                tab.isVisible = visible;
-                if (tab.isActive) {
-                    // If tab is active, set a different one as active.
-                    tab.isActive = visible;
-                    this.tabs.every((tab, index, array) => {
-                        if (tab.isVisible) {
-                            tab.isActive = true;
-                            return false;
-                        }
-                        return true;
-                    });
-                }
-            },
-        },
-    };
+import expiringStorage from "./expiringStorage";
+export default {
+  props: {
+    cacheLifetime: {
+      default: 5
+    },
+    options: {
+      type: Object,
+      required: false,
+      default: () => ({
+        useUrlFragment: true
+      })
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    tabs: [],
+    activeTabHash: ""
+  }),
+  computed: {
+    storageKey() {
+      return `vue-tabs-component.cache.${window.location.host}${
+        window.location.pathname
+      }`;
+    }
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  mounted() {
+    window.addEventListener("hashchange", () =>
+      this.selectTab(window.location.hash)
+    );
+    if (this.findTab(window.location.hash)) {
+      this.selectTab(window.location.hash);
+      return;
+    }
+    const previousSelectedTabHash = expiringStorage.get(this.storageKey);
+    if (this.findTab(previousSelectedTabHash)) {
+      this.selectTab(previousSelectedTabHash);
+      return;
+    }
+    if (this.tabs.length) {
+      this.selectTab(this.tabs[0].hash);
+    }
+  },
+  methods: {
+    findTab(hash) {
+      return this.tabs.find(tab => tab.hash === hash);
+    },
+    selectTabAction(selectedTabHash, event){
+        if(!this.readonly)
+            this.selectTab(selectedTabHash,event)
+    },
+    selectTab(selectedTabHash, event) {
+      // See if we should store the hash in the url fragment.
+      if (event && !this.options.useUrlFragment) {
+        event.preventDefault();
+      }
+      const selectedTab = this.findTab(selectedTabHash);
+      if (!selectedTab) {
+        return;
+      }
+      if (selectedTab.isDisabled) {
+        return;
+      }
+      this.tabs.forEach(tab => {
+        tab.isActive = tab.hash === selectedTab.hash;
+      });
+      this.$emit("changed", { tab: selectedTab });
+      this.activeTabHash = selectedTab.hash;
+      expiringStorage.set(
+        this.storageKey,
+        selectedTab.hash,
+        this.cacheLifetime
+      );
+    },
+    selectTabByIndex(i) {
+      if (i < 0 || i > this.tabs.length) {
+        throw "Index out of bounds";
+        return;
+      }
+      let tab = this.tabs[i];
+      this.selectTab(tab.hash);
+    },
+    setTabVisible(hash, visible) {
+      const tab = this.findTab(hash);
+      if (!tab) {
+        return;
+      }
+      tab.isVisible = visible;
+      if (tab.isActive) {
+        // If tab is active, set a different one as active.
+        tab.isActive = visible;
+        this.tabs.every((tab, index, array) => {
+          if (tab.isVisible) {
+            tab.isActive = true;
+            return false;
+          }
+          return true;
+        });
+      }
+    }
+  }
+};
 </script>
