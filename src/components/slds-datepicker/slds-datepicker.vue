9ce873fa59e551dd78c438baa971f07a40305538
Guto <luiz.v.nasc@gmail.com>
2018-05-20 17:37:59 -0300

    começado a criação do datepicker

---
+++ src/components/slds-datepicker/slds-datepicker.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="slds-form-element__control slds-input-has-icon slds-input-has-icon_right" 
+    :class="{'slds-dropdown-trigger_click slds-is-open': isOpen}">
+      <input type="text" id="date-input-id" placeholder=" " class="slds-input" value="06/24/2014" />
+      <button class="slds-button slds-button_icon slds-input__icon slds-input__icon_right" :title="config.i18n.tip" @click="openDialog()">
+        <event-icon class="slds-button__icon" aria-hidden="true"/>
+        <span class="slds-assistive-text">{{config.i18n.tip}}</span>
+      </button>
+      <div v-if="isOpen" v-click-outside="closeDialog" aria-hidden="false" aria-label="Date picker: June" class="slds-datepicker slds-dropdown slds-dropdown_left" role="dialog">
+      <div class="slds-datepicker__filter slds-grid">
+        <div class="slds-datepicker__filter_month slds-grid slds-grid_align-spread slds-grow">
+          <div class="slds-align-middle">
+            <button class="slds-button slds-button_icon slds-button_icon-container" :title="config.i18n.previous_month" @click="previous()">
+              <left-icon class="slds-button__icon" aria-hidden="true"/>
+              <span class="slds-assistive-text">{{config.i18n.previous_month}}</span>
+            </button>
+          </div>
+          <h2 aria-atomic="true" aria-live="assertive" class="slds-align-middle" id="month">{{currentMonthName}}</h2>
+          <div class="slds-align-middle">
+            <button class="slds-button slds-button_icon slds-button_icon-container" :title="config.i18n.next_month" @click="next()">
+              <right-icon class="slds-button__icon" aria-hidden="true"/>
+              <span class="slds-assistive-text">{{config.i18n.next_month}}</span>
+            </button>
+          </div>
+        </div>
+        <div class="slds-shrink-none">
+          <label class="slds-assistive-text" for="select-01">{{config.i18n.pick_year}}</label>
+          <div class="slds-select_container">
+            <select class="slds-select" id="select-01" v-model="year">
+              <option v-for="year in years" :key="year" :value="year">{{year}}</option>
+            </select>
+          </div>
+        </div>
+        <slds-month-table/>
+      </div>
+    </div>
+</template>
+<script>
+import eventIcon from "../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg?event";
+import leftIcon from "../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg?left";
+import rightIcon from "../../../node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg?right";
+import _ from "lodash";
+import moment from "moment";
+import ClickOutside from "vue-click-outside";
+import SldsMonthTable from './slds-month-table'
+export default {
+  directives: { ClickOutside },
+  props: {
+    config: {
+      type: Object,
+      default: () => {
+        return {
+          i18n: {
+            tip: "Select a date",
+            next_month: "Next Month",
+            previous_month: "Previous Month",
+            pick_year: "Pick a Year",
+            months: [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December"
+            ]
+          }
+        };
+      }
+    }
+  },
+  components: {
+    "event-icon": eventIcon,
+    "left-icon": leftIcon,
+    "right-icon": rightIcon,
+    'slds-month-table': SldsMonthTable
+  },
+  data: function() {
+    return {
+      isOpen: false,
+      month: 0,
+      year: moment().year()
+    };
+  },
+  computed: {
+    years: function() {
+      return _.range(1900, 2999);
+    },
+    currentMonthName: function() {
+      return this.config.i18n.months[this.month];
+    }
+  },
+  methods: {
+    next: function() {
+      if (this.month == 11) {
+        if (this.year === 2099) this.year = 1900;
+        else this.year++;
+        this.month = 0;
+      } else {
+        this.month++;
+      }
+    },
+    previous: function() {
+      if (this.month === 0) {
+        if (this.year === 1900) this.year = 2099;
+        else this.year--;
+        this.month = 11;
+      } else {
+        this.month--;
+      }
+    },
+    closeDialog: function(event) {
+        this.isOpen = false;
+    },
+    openDialog: function() {
+      console.log("abrir popup");
+      this.isOpen = true;
+    }
+  }
+};
+</script>
---
+++ src/components/slds-datepicker/slds-month-table.vue
@@ -0,0 +1,190 @@
+<template>
+    <table aria-labelledby="month" aria-multiselectable="true" class="slds-datepicker__month" role="grid">
+        <thead>
+            <tr id="weekdays">
+            <th id="Sunday" scope="col">
+                <abbr title="Sunday">{{options.daysOfWeekInitials[0]}}</abbr>
+            </th>
+            <th id="Monday" scope="col">
+                <abbr title="Monday">{{options.daysOfWeekInitials[1]}}</abbr>
+            </th>
+            <th id="Tuesday" scope="col">
+                <abbr title="Tuesday">{{options.daysOfWeekInitials[2]}}</abbr>
+            </th>
+            <th id="Wednesday" scope="col">
+                <abbr title="Wednesday">{{options.daysOfWeekInitials[3]}}</abbr>
+            </th>
+            <th id="Thursday" scope="col">
+                <abbr title="Thursday">{{options.daysOfWeekInitials[4]}}</abbr>
+            </th>
+            <th id="Friday" scope="col">
+                <abbr title="Friday">{{options.daysOfWeekInitials[5]}}</abbr>
+            </th>
+            <th id="Saturday" scope="col">
+                <abbr title="Saturday">{{options.daysOfWeekInitials[6]}}</abbr>
+            </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td aria-disabled="true" aria-selected="false" class="slds-disabled-text" role="gridcell">
+                <span class="slds-day">31</span>
+            </td>
+            <td aria-selected="false" role="gridcell" tabindex="0">
+                <span class="slds-day">1</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">2</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">3</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">4</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">5</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">6</span>
+            </td>
+            </tr>
+            <tr>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">7</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">8</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">9</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">10</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">11</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">12</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">13</span>
+            </td>
+            </tr>
+            <tr>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">14</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">15</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">16</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">17</span>
+            </td>
+            <td aria-selected="false" aria-current="date" class="slds-is-today" role="gridcell">
+                <span class="slds-day">18</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">19</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">20</span>
+            </td>
+            </tr>
+            <tr class="">
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">21</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">22</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">23</span>
+            </td>
+            <td aria-selected="false" class="" role="gridcell">
+                <span class="slds-day">24</span>
+            </td>
+            <td aria-selected="false" class="" role="gridcell">
+                <span class="slds-day">25</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">26</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">27</span>
+            </td>
+            </tr>
+            <tr class="">
+            <td aria-selected="false" class="" role="gridcell">
+                <span class="slds-day">28</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">29</span>
+            </td>
+            <td aria-selected="false" role="gridcell">
+                <span class="slds-day">30</span>
+            </td>
+            <td aria-disabled="true" aria-selected="false" class="slds-disabled-text" role="gridcell">
+                <span class="slds-day">1</span>
+            </td>
+            <td aria-disabled="true" aria-selected="false" class="slds-disabled-text" role="gridcell">
+                <span class="slds-day">2</span>
+            </td>
+            <td aria-disabled="true" aria-selected="false" class="slds-disabled-text" role="gridcell">
+                <span class="slds-day">3</span>
+            </td>
+            <td aria-disabled="true" aria-selected="false" class="slds-disabled-text" role="gridcell">
+                <span class="slds-day">4</span>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+</template>
+<script>
+import moment from "moment";
+export default {
+  props: {
+    options: {
+      type: Object,
+      default: () => {
+        return {
+          daysOfWeekInitials: {
+            type: Array,
+            default:() =>{ return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
+          },
+          months: {
+            type: Array,
+            default: function(){
+                return [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "Octuber",
+              "November",
+              "December"
+            ]
+            }
+          }
+        };
+      }
+    }
+  },
+  data: function() {
+    return {
+      month: moment().month(),
+      year: moment().year(),
+      day: moment().day()
+    };
+  }
+};
+</script>
