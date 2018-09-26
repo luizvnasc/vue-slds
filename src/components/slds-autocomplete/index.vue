1214b64cbc89aa94660a03efa7a6b41f0f2a63eb
Guto <luiz.v.nasc@gmail.com>
2018-09-26 13:36:41 -0300

    correção da seleção no autocomplete

--- src/components/slds-autocomplete/index.vue
+++ src/components/slds-autocomplete/index.vue
@@ -1,46 +1,28 @@
 <template>
 	<div class="slds-form-element__control">
 		<div class="slds-combobox_container">
-			<div 
-				class="slds-combobox slds-dropdown-trigger" 
-				aria-expanded="false" 
-				aria-haspopup="listbox" 
-				role="combobox">
-				<div 
-					class="slds-combobox__form-element slds-input-has-icon slds-input-has-icon_right" 
-					role="none">
-					<input 
-						id="combobox-id-1" 
-						v-model="value" 
-						type="text" 
-						class="slds-input slds-combobox__input" 
-						aria-autocomplete="list" 
-						aria-controls="listbox-id-1" 
-						autocomplete="off" 
-						role="textbox" 
-						@focus="focus()" 
-						@blur="blur()" 
+			<div class="slds-combobox slds-dropdown-trigger" aria-expanded="false" aria-haspopup="listbox" role="combobox">
+				<div class="slds-combobox__form-element slds-input-has-icon slds-input-has-icon_right" role="none">
+					<input
+						id="combobox-id-1"
+						v-model="value"
+						type="text"
+						class="slds-input slds-combobox__input"
+						aria-autocomplete="list"
+						aria-controls="listbox-id-1"
+						autocomplete="off"
+						role="textbox"
+						@focus="focus()"
+						@blur="blur()"
 @input="update()" >
 					<span class="slds-icon_container slds-icon-utility-search slds-input__icon slds-input__icon_right">
-						<search-icon/>
+						<search-icon />
 					</span>
 				</div>
-				<div 
-					v-show="hasFocus" 
-					id="listbox-id-1" 
-					class="slds-dropdown slds-dropdown_length-7 slds-dropdown_fluid" 
-					role="listbox">
-					<ul 
-						class="slds-listbox slds-listbox_vertical" 
-						role="presentation">
-						<li 
-							v-for="(data,key) in dataset" 
-							:key="key" 
-							class="slds-listbox__item" 
-							@click="setResult(data)">
-							<div 
-								class="slds-media slds-listbox__option slds-listbox__option_entity slds-listbox__option_has-meta" 
-								role="option" >
+				<div v-show="hasFocus" id="listbox-id-1" class="slds-dropdown slds-dropdown_length-7 slds-dropdown_fluid" role="listbox">
+					<ul class="slds-listbox slds-listbox_vertical" role="presentation">
+						<li v-for="(data,key) in dataset" :key="key" class="slds-listbox__item" @click="setResult(data)">
+							<div class="slds-media slds-listbox__option slds-listbox__option_entity slds-listbox__option_has-meta" role="option">
 								<span class="slds-media__body slds-clearfix">
 									<span class="slds-float_left">{{ data }}</span>
 								</span>
@@ -84,7 +66,10 @@ export default {
 		},
 		blur() {
 			var self = this
-			setTimeout(function(){ self.hasFocus = false},100)
+			setTimeout(function(){
+				self.hasFocus = false
+			
+			},200)
 			this.$emit('blur')        
 		},  
 		update() {
