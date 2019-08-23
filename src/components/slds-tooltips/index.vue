082213309714a2197f12f3deb3bf4bbe245b4ded
Guto <luiz.v.nasc@gmail.com>
2019-08-23 12:39:48 -0300

    Change the tooltip to show on hover

--- src/components/slds-tooltips/index.vue
+++ src/components/slds-tooltips/index.vue
@@ -1,46 +1,46 @@
 <template>
-	<div class="slds-form-element__icon">
-		<button class="slds-button slds-button_icon slds-button slds-button_icon" aria-describedby="help" title="Help" @click="click()">
-			<info-icon class="slds-button__icon" />
-		</button>
-		<div v-show="open" class="slds-popover slds-popover_tooltip slds-nubbin_bottom-left vslds-tooltip" role="tooltip" id="help">
-			<div class="slds-popover__body">{{tooltip}}</div>
-		</div>
-	</div>
+    <div class="slds-form-element__icon">
+        <button class="slds-button slds-button_icon slds-button slds-button_icon" aria-describedby="help" title="Help" @mouseover="show()">
+            <info-icon class="slds-button__icon" />
+        </button>
+        <div v-show="open" class="slds-popover slds-popover_tooltip slds-nubbin_bottom-left vslds-tooltip" role="tooltip" id="help">
+            <div class="slds-popover__body">{{tooltip}}</div>
+        </div>
+    </div>
 </template>
 
 <script>
-import InfoIcon from 'node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg?info'
+import InfoIcon from "node_modules/@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg?info";
 export default {
-	components:{
-		'info-icon':InfoIcon
-	},
-	props:{
-		tooltip:{
-			type: String,
-			default:undefined
-		}
-	},
-	data(){
-		return {
-			open:false
-		}
-	},
-	methods:{
-		click(){
-			this.open = !this.open
-		}
-	}
-}
+  components: {
+    "info-icon": InfoIcon
+  },
+  props: {
+    tooltip: {
+      type: String,
+      default: undefined
+    }
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  methods: {
+    show() {
+      this.open = !this.open;
+    }
+  }
+};
 </script>
 
 <style>
 .vslds-tooltip {
-    position: absolute;
-    top: -45px;
-    left: -15px;
-    width: 100%;
-    max-width: 300px;
-    min-width: 200px;
+  position: absolute;
+  top: -45px;
+  left: -15px;
+  width: 100%;
+  max-width: 300px;
+  min-width: 200px;
 }
 </style>
